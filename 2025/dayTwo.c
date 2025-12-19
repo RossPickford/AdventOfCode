3d35de95f79a537ccd9d5701df7e9c9061549554
VND Pickford <Pickford.Ross@bcg.com>
2025-12-19 17:31:22 +0000

    continuing with D2 P1

--- 2025/dayTwo.c
+++ 2025/dayTwo.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <limits.h>
 #include <math.h>
 
-unsigned int partOne(unsigned int *ans);
+unsigned long long partOne(unsigned long long *ans);
 int getDigitCount(unsigned int num);
+unsigned long long powll(unsigned int x, unsigned int y);
 
 int main(void)
 {
-    unsigned int ans = 0;
+    unsigned long long ans = 0;
 
-    // FILE *input = fopen("DayTwo_TestInput.txt", "r");
-    FILE *input = fopen("DayTwo_Input.txt", "r");
+    FILE *input = fopen("DayTwo_TestInput.txt", "r");
+    // FILE *input = fopen("DayTwo_Input.txt", "r");
 
     char c;
-    unsigned int range[2];
+    unsigned long long range[2];
     unsigned int val = 0;
 
     while ((c = fgetc(input)) != EOF)
@@ -32,53 +34,55 @@ int main(void)
             val = 0;
 
             ans += partOne(range);
+            // printf("Total: %llu\n", ans);
         }
     }
 
     range[1] = val;
     ans += partOne(range);
 
-    printf("\n%u\n", ans);
+    printf ("\n%llu\n", ans);
 
     return 0;
 }
 
-unsigned int partOne(unsigned int *input)
+unsigned long long partOne(unsigned long long *input)
 {
-    printf("%u, %u\n", *input, *(input + 1));
-
+    
     int inDigCount_1 = getDigitCount(*input);
     int inDigCount_2 = getDigitCount(*(input + 1));
-
-    if (inDigCount_1 % 2 != 0 && inDigCount_2 % 2 != 0)
-        return 0;
-
+    
+    // if (inDigCount_1 % 2 != 0 && inDigCount_2 % 2 != 0)
+    //     return 0;
+    
     if (inDigCount_1 % 2 != 0)
-        *input = pow(10, --inDigCount_1);
+    *input = pow(10, ++inDigCount_1);
     else if (inDigCount_2 % 2 != 0)
-        *(input + 1) = pow(10, --inDigCount_2) - 1;
+    *(input + 1) = powll(10, inDigCount_2--) - 1;
+    
+    printf("%llu, %llu\n", *input, *(input + 1));
 
     inDigCount_1 /= 2;
     inDigCount_2 /= 2;
+    
+    unsigned long long total = 0;
 
-    unsigned int total = 0;
+    unsigned int inFirstSeg_1 = *input / (pow(10, inDigCount_1));
+    unsigned int inFirstSeg_2 = *(input + 1) / (pow(10, inDigCount_2));
 
-    int inFirstSeg_1 = *input / (pow(10, inDigCount_1));
-    int inFirstSeg_2 = *(input + 1) / (pow(10, inDigCount_2));
+    printf("segments: %u, %u\n", inFirstSeg_1, inFirstSeg_2);
 
-    printf("%u, %u\n", inFirstSeg_1, inFirstSeg_2);
-    int first = 0;
     for (; inFirstSeg_1 <= inFirstSeg_2; inFirstSeg_1++)
     {
-        unsigned int val = inFirstSeg_1 + (inFirstSeg_1 * pow(10, getDigitCount(inFirstSeg_1)));
+        unsigned long long val = inFirstSeg_1 + (inFirstSeg_1 * pow(10, getDigitCount(inFirstSeg_1)));
 
         if (val >= *input && val <= *(input + 1))
         {
             total += val;
-            printf("%u\n", val);
+            printf("%llu\n", val);
         }
     }
-
+    // printf("%u\n", total);
     return total;
 }
 
@@ -89,4 +93,13 @@ int getDigitCount(unsigned int num)
         ;
 
     return ++total;
+}
+
+unsigned long long powll(unsigned int x, unsigned int y)
+{
+    unsigned long ans = x;
+    for(int i = 1; i < y; i++)
+    ans *= x;
+
+    return ans;
 }
